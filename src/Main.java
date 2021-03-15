53ebbbb8024ddba79bc2f5fa26f9dcaa774b9a2e
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-03-15 18:04:14 +0500

    B

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int current_balance = 100;
+        int currentBalance = 100;
         System.out.print("Пожалуйста, введите сумму пополнения: ");
-        int replenishment_amount = in.nextInt();
+        int replenishmentAmount = in.nextInt();
         int account_amount;
-        boolean b = 1001 > replenishment_amount;
+        boolean b = 1000 < replenishmentAmount;
         if (b) {
-            account_amount = current_balance + replenishment_amount;
-            System.out.println("Ваш текущий баланс: " + account_amount + " рублей.");
+            int bonusAmount;
+            bonusAmount = replenishmentAmount/100;
+            account_amount = bonusAmount + replenishmentAmount + currentBalance;
+            System.out.println("Ваш текущий баланс: " + account_amount + " рублей, " + "бонус за пополнение: " + bonusAmount + " рублей.");
         } else {
-            int bonus_amount;
-            bonus_amount = replenishment_amount/100;
-            account_amount = bonus_amount + replenishment_amount + current_balance;
-            System.out.println("Ваш текущий баланс: " + account_amount + " рублей, " + "бонус за пополнение: " + bonus_amount + " рублей.");
+            account_amount = currentBalance + replenishmentAmount;
+            System.out.println("Ваш текущий баланс: " + account_amount + " рублей.");
         }
     }
 }
